6751563d82fea75396eabce568929d45289cfe49
John Ingham <johnaingham@yahoo.co.uk>
2021-09-23 11:39:26 +0100

    Code up to heroku

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,19 @@ const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or URL DATABASE not set');
 }
-const db = new Pool()
+const db = new Pool(config)
 
 module.exports = db;
--- listen.js
+++ listen.js
@@ -4,7 +4,9 @@ const server = http.createServer((req, res) => {
     //console.log(request)
 })
 
-server.listen(9090, (err) =>{
+const PORT = process.env.PORT || 9090
+
+server.listen(PORT, (err) =>{
     if (err) console.log(err)
-    else console.log("Server listens at Port 9090")
+    else console.log(`Server listens at Port ${PORT}`)
 })
