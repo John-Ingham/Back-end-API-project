29a3c678ef284f4995284728f5ed57ade2bfbf6e
John Ingham <johnaingham@yahoo.co.uk>
2021-10-18 12:49:01 +0100

    edits

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,44 +1,50 @@
 const db = require('../connection.js')
 //const {categoryData, commentData, reviewData, userData} = require('../data/development-data/index')
-const format = require('pg-format');
-const { formatUserData, formatCategoryData, formatReviewData, formatCommentsData } = require('../utils/data-manipulation.js');
+const format = require('pg-format')
+const {
+  formatUserData,
+  formatCategoryData,
+  formatReviewData,
+  formatCommentsData,
+} = require('../utils/data-manipulation.js')
 
 const seed = async (data) => {
-  const { categoryData, commentData, reviewData, userData } = data;
+  const { categoryData, commentData, reviewData, userData } = data
   // Create tables
-   const dropTables = async () => {
-     await db.query(`DROP TABLE IF EXISTS comments;`)
-     await db.query(`DROP TABLE IF EXISTS reviews;`)
-     await db.query(`DROP TABLE IF EXISTS categories;`)
-     await db.query(`DROP TABLE IF EXISTS users;`)
- 
-   }
+  const dropTables = async () => {
+    await db.query(`DROP TABLE IF EXISTS comments;`)
+    await db.query(`DROP TABLE IF EXISTS reviews;`)
+    await db.query(`DROP TABLE IF EXISTS categories;`)
+    await db.query(`DROP TABLE IF EXISTS users;`)
+  }
   await dropTables()
-  
-   return db.query(`CREATE TABLE categories (
+
+  return (
+    db
+      .query(
+        `CREATE TABLE categories (
    slug VARCHAR (500) PRIMARY KEY NOT NULL,
    description VARCHAR (500) NOT NULL
-);`)
+);`,
+      )
 
-.then(() => {
-  return db
-  .query(`
+      .then(() => {
+        return db.query(`
   CREATE TABLE users (
     username VARCHAR (500) PRIMARY KEY NOT NULL,
     avatar_url VARCHAR (500),
     name VARCHAR (500) NOT NULL
   )`)
-})
+      })
 
-.then(() => {
-  return db
-    .query(`
+      .then(() => {
+        return db.query(`
     CREATE TABLE reviews (
     review_id SERIAL PRIMARY KEY,
     title VARCHAR (500) NOT NULL,
     review_body VARCHAR (1000) NOT NULL,
     designer VARCHAR (500) NOT NULL,
-    review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+    review_image_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
     votes INT DEFAULT 0,
     category VARCHAR (500),
     owner VARCHAR (500),
@@ -46,11 +52,10 @@ const seed = async (data) => {
     FOREIGN KEY (owner) REFERENCES users(username),
     FOREIGN KEY (category) REFERENCES categories(slug)
     );`)
-  })
+      })
 
-  .then(() => {
-    return db
-    .query(`
+      .then(() => {
+        return db.query(`
       CREATE TABLE comments (
       comment_id SERIAL PRIMARY KEY,
       author VARCHAR (500) REFERENCES users(username),
@@ -59,43 +64,43 @@ const seed = async (data) => {
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       body VARCHAR (500) NOT NULL
     );`)
-  })
-  
-  // Insert data
-  .then(()=> {
-    const formattedUserData = formatUserData(userData)
-    const shopInsertion = format(
-      `INSERT INTO users (username, avatar_url, name) VALUES %L RETURNING *;`,
-      formattedUserData
-    )
-    return db.query(shopInsertion)
-  })
-  .then(() =>{
-    const formattedCategoryData = formatCategoryData(categoryData)
-    const categoryInsertion = format(
-      `INSERT INTO categories (slug, description) VALUES %L RETURNING *;`,
-      formattedCategoryData
-    )
-    return db.query(categoryInsertion)
-  })
-  .then (() => {
-    const formattedReviewData = formatReviewData(reviewData)
-    
-    const reviewInsertion =format(
-      `INSERT INTO reviews (title, review_body, designer, review_img_url, votes, category, owner, created_at) VALUES %L RETURNING *;`,
-      formattedReviewData
-    )
-    return db.query(reviewInsertion)
-  })
-  .then(() => {
-    const formattedCommentData = formatCommentsData(commentData)
-    const commentInsertion = format(
-      `INSERT INTO comments (author, review_id, votes, created_at, body) VALUES %L RETURNING *;`,
-      formattedCommentData
-    )
-    return db.query(commentInsertion)
-  })
+      })
 
-} 
+      // Insert data
+      .then(() => {
+        const formattedUserData = formatUserData(userData)
+        const shopInsertion = format(
+          `INSERT INTO users (username, avatar_url, name) VALUES %L RETURNING *;`,
+          formattedUserData,
+        )
+        return db.query(shopInsertion)
+      })
+      .then(() => {
+        const formattedCategoryData = formatCategoryData(categoryData)
+        const categoryInsertion = format(
+          `INSERT INTO categories (slug, description) VALUES %L RETURNING *;`,
+          formattedCategoryData,
+        )
+        return db.query(categoryInsertion)
+      })
+      .then(() => {
+        const formattedReviewData = formatReviewData(reviewData)
 
-module.exports = seed;
+        const reviewInsertion = format(
+          `INSERT INTO reviews (title, review_body, designer, review_img_url, votes, category, owner, created_at) VALUES %L RETURNING *;`,
+          formattedReviewData,
+        )
+        return db.query(reviewInsertion)
+      })
+      .then(() => {
+        const formattedCommentData = formatCommentsData(commentData)
+        const commentInsertion = format(
+          `INSERT INTO comments (author, review_id, votes, created_at, body) VALUES %L RETURNING *;`,
+          formattedCommentData,
+        )
+        return db.query(commentInsertion)
+      })
+  )
+}
+
+module.exports = seed
--- models/reviewArray.model.js
+++ models/reviewArray.model.js
@@ -1,32 +1,47 @@
 const db = require('../db/connection')
 
-exports.fetchReviewArray = (sort_by = "created_at", order = "desc", category) => {
-    if (!['', 'review_id', 'title', 'designer', 'votes', 'category', 'owner', 'created_at'].includes(sort_by)){
-        return Promise.reject({status: 400, msg: "Invalid sort query"})
-    }
-    if (!['asc', 'desc'].includes(order)){
-        return Promise.reject({status: 400, msg:"Invalid order query"})
-    }
-    let queryStr = `
-    SELECT reviews.review_id, title, designer, review_img_url, reviews.votes, category, owner, reviews.created_at,
+exports.fetchReviewArray = (
+  sort_by = 'created_at',
+  order = 'desc',
+  category,
+) => {
+  if (
+    ![
+      '',
+      'review_id',
+      'title',
+      'designer',
+      'votes',
+      'category',
+      'owner',
+      'created_at',
+    ].includes(sort_by)
+  ) {
+    return Promise.reject({ status: 400, msg: 'Invalid sort query' })
+  }
+  if (!['asc', 'desc'].includes(order)) {
+    return Promise.reject({ status: 400, msg: 'Invalid order query' })
+  }
+  let queryStr = `
+    SELECT reviews.review_id, title, designer, review_image_url, reviews.votes, category, owner, reviews.created_at,
     COUNT(comment_id) AS comment_count 
     FROM reviews
     LEFT OUTER JOIN comments ON comments.review_id = reviews.review_id
     `
-    let queryValues = []
-    if (category) {
-        if(!['euro game', 'social deduction', 'dexterity'].includes(category)){
-            return Promise.reject({status: 404, msg: "Category not found"})
-        }
-        queryValues.push(category)
-        queryStr += `WHERE category=$1`
-        
+  let queryValues = []
+  if (category) {
+    if (!['euro game', 'social deduction', 'dexterity'].includes(category)) {
+      return Promise.reject({ status: 404, msg: 'Category not found' })
     }
-    queryStr += `GROUP BY reviews.review_id ORDER BY ${sort_by} ${order}`
+    queryValues.push(category)
+    queryStr += `WHERE category=$1`
+  }
+  queryStr += `GROUP BY reviews.review_id ORDER BY ${sort_by} ${order}`
 
-    return db.query(queryStr, queryValues)
+  return db
+    .query(queryStr, queryValues)
 
-    .then((result) =>{       
-        return result.rows
+    .then((result) => {
+      return result.rows
     })
 }
